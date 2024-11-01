65212a27729718d78552d96a8a32ae661604c2b3
bhrtgbrt <chengandrew0513@gmail.com>
2024-11-01 17:39:19 +0800

    1101

---
+++ Lab4/app/src/main/java/com/example/lab4/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.lab4
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private val startForResult = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result: ActivityResult ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val intent = result.data
+            val drink = intent?.getStringExtra("drink")
+            val sugar = intent?.getStringExtra("sugar")
+            val ice = intent?.getStringExtra("ice")
+            val tvMeal = findViewById<TextView>(R.id.tvMeal)
+            tvMeal.text = "飲料：$drink\n\n甜度：$sugar\n\n冰塊：$ice"
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        val btnChoice = findViewById<Button>(R.id.btnChoice)
+        btnChoice.setOnClickListener {
+            val intent = Intent(this, SecActivity::class.java)
+            startForResult.launch(intent)
+        }
+    }
+}
---
+++ Lab5/app/src/main/java/com/example/lab5/ViewPagerAdapter.kt
@@ -0,0 +1,22 @@
+package com.example.lab5
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter(
+    fm: FragmentManager,
+    lifecycle: Lifecycle
+) : FragmentStateAdapter(fm, lifecycle) {
+
+    override fun getItemCount(): Int = 3
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> FirstFragment()
+            1 -> SecondFragment()
+            else -> ThirdFragment()
+        }
+    }
+}
